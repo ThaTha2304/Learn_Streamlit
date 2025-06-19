ad118a1ef3be2618cd1eb472e7197a2fdf3b34a1
ThaTha2304 <taufiqagung23@gmail.com>
2025-06-19 10:30:09 +0700

    Display data, using magic commands

---
+++ LearnStreamlit.py
@@ -0,0 +1,13 @@
+"""
+    # My First App
+    Here's our first attempt at using data to create a table
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+    'first_column' : [1,2,3,4],
+    'second_column' : [10,20,30,40] 
+})
+
+df
